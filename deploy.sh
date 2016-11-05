d7688515c3bdb51bf81fd803b9a5dc39dbe12279
Marcus Pasell <rickyrombo@gmailcom>
2016-11-04 21:23:28 -0500

    rebuilding site Fri, Nov  4, 2016  9:23:28 PM

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,11 @@ git add -A
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
-  fi
-  git commit -m "$msg"
+fi
+git commit -m "$msg"
 
-  # Push source and build repos.
-  git push origin master
+# Push source and build repos.
+git push origin master
 
-  # Come Back
-  cd ..
+# Come Back
+cd ..
