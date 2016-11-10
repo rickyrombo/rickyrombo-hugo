5a8847238cf2a030579fcf1c75dbb4b7cc5e2042
Marcus Pasell <rickyrombo@gmail.com>
2016-11-09 23:23:38 -0600

    removed requirejs modules

--- static/js/app.js
+++
@@ -1,9 +0,0 @@
-define('app', ['jquery', 'underscore', 'backbone', 'router', 'album_art', 'nav'],
-    function($, _, Backbone, Router) {
-        var init = function() {
-            var router = new Router();
-            router.init();
-        };
-        return { init: init };
-    }
-)
