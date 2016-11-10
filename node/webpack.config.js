446cf313f9c2fd8a52333e4bf93de1f4963716dd
Marcus Pasell <rickyrombo@gmail.com>
2016-11-10 04:17:15 -0600

    added production env

--- node/webpack.config.js
+++ node/webpack.config.js
@@ -18,5 +18,12 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
 }
