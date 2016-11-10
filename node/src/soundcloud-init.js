5d156f8aba3faf3f8250530313ec2a42683249ff
Marcus Pasell <rickyrombo@gmail.com>
2016-11-10 01:45:34 -0600

    callback pls work

--- node/src/soundcloud-init.js
+++ node/src/soundcloud-init.js
@@ -3,6 +3,6 @@ import SC from 'soundcloud';
 SC.initialize({
     client_id: '4790864defb6a0d7eb3017d49a31b273',
     redirect_uri: window.location.protocol + '//rickyrombo.github.io/callback'
-});
+})
 
-module.exports = SC;
+module.exports = SC
