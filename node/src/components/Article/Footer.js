169b05da5427bb5e21c15d0e4978111368ef0851
Marcus Pasell <rickyrombo@gmail.com>
2016-11-10 00:05:19 -0600

    cosmetic changes, dynamic footer date

--- node/src/components/Article/Footer.js
+++ node/src/components/Article/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component
         return (
             <footer id="footer" className="footer">
                 <p>All cover art and music is provided using <a target="_blank" title="Soundcloud.com" href="http://soundcloud.com">Soundcloud</a>. Background cover art are those of <a target="_blank" title="View on Soundcloud" href="http://soundcloud.com/rickyrombo/favorites">RickyRombo's favorites</a></p>
-                <p>Copyright &copy;2015 Marcus Pasell - All Rights Reserved</p>
+                <p>Copyright &copy; {(new Date()).getFullYear()} Marcus Pasell - All Rights Reserved</p>
             </footer>
         )
     }
